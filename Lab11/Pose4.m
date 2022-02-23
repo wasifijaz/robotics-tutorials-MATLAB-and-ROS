8e0013fd4f12145e45f1e177876a3fdc22e7a9a1
wasifijaz <62423571+wasifijaz@users.noreply.github.com>
2022-02-24 04:43:13 +0500

    Robotics Lab 11
    Developing the Kinematic Model of a 4-DOF Manipulator SCARA

---
+++ Lab11/Pose4.m
@@ -0,0 +1,13 @@
+clc 
+clear all 
+Ll =0.250; 
+L2 =0.350; 
+d1 =0.300;
+L(1)= Link([d1 Ll 0 -180]);
+L(2)= Link([0 L2 0 -90]);
+L(3)= Link([0 0 180 100]);
+L(4)= Link([0 0 0 90]);
+RR = SerialLink(L);
+t = fkine(RR ,[0 0 0 0])
+RR.plot([-250 350 200 0])
+Q = RR.ikine (t, '[0 0 0 0]', 'mask', [1 1 1 1 0 0])
